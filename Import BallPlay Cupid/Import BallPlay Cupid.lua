7f86d15ce76c39e011fb1683ecb72c9311c35241
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-10-07 22:23:17 +0200

    vUpdate

--- Import BallPlay Cupid/Import BallPlay Cupid.lua
+++ Import BallPlay Cupid/Import BallPlay Cupid.lua
@@ -21,7 +21,7 @@
 -- Please note that some references to data like pictures or audio, do not automatically
 -- fall under this licenses. Mostly this is noted in the respective files.
 -- 
--- Version: 22.09.28
+-- Version: 22.10.07
 -- </License Block>
 -- Script
 local load=load
